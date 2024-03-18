16529727548963f964dc7bd13ab4e10c4b3c9d1d
Kássio Silva <kassio.slva@gmail.com>
2024-03-18 18:52:53 -0300

    fix: search by cpf

--- src/main/java/com/example/registrodeclientes/RegisterClientController.java
+++ src/main/java/com/example/registrodeclientes/RegisterClientController.java
@@ -5,11 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Controller
@@ -24,17 +22,16 @@ public class RegisterClientController {
 
   @GetMapping("/clients")
   public String getClient(String cpf, Model model) {
-    if (cpf != null && cpf.isEmpty()) {
-      System.out.println("AQUI");
+    if (cpf != null && !cpf.isEmpty()) {
       List<Client> filteredClients = clients.stream().
               filter(client -> client.getCpf().equals(cpf)).collect(Collectors.toList());
 
       model.addAttribute("clients", filteredClients);
     } else {
-      System.out.println("VAZIO!");
       model.addAttribute("clients", clients);
     }
 
+    model.addAttribute("cpf", cpf);
     return "result";
   }
 
